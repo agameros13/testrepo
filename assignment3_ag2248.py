d08483a78197b3053f78aac8c4ea3150b78d0970
Andrea Gameros <120198743+agameros13@users.noreply.github.com>
2024-03-14 13:15:43 -0500

    Update assignment3_ag2248.py

--- assignment3_ag2248.py
+++ assignment3_ag2248.py
@@ -1,5 +1,8 @@
-def main(){
-    print('this is my main')
-}
-
+def main():
+    num1 = int(input('Give me your first number: '))
+    num2 = int(input('Give me your second number: '))
+    print(f'The result is {math(num1,num2)}')
+def math(number1,number2):
+    multiplication = number1*number2
+    return multiplication
 main()
